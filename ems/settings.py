5f9dae261db58d2bcb795edf5e7ad41def94533a
srilekhabayyapu <srilekhabayyapu313@gmail.com>
2025-11-29 21:23:08 +0530

    hosts are changed here

--- ems/settings.py
+++ ems/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-s08(!ybg5jtb%z(%p#fwwkz&)mcu5f9awsh36k#f**-j*rvp9+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ems-xumz.onrender.com/"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
 
 
 # Application definition
